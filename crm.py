fc18e24ba299098ab46d9262b260ea16984df4c2
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-11-13 19:01:14 +0300

    get sales add

--- crm.py
+++ crm.py
@@ -439,7 +439,7 @@ class CRMExtractor:
         # Выбор элемента из выпадающего списка
         select = Select(menu_item)
         select.select_by_value(str(self.end_date.month))
-        time.sleep(3)
+        time.sleep(4)
 
         print('Нажимаю ОБНОВИТЬ ДАННЫЕ')
         menu_item = wait.until(
