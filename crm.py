7fdd329d45599cd35cb8846dbb9a7665df09a149
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-11-14 08:28:45 +0300

    get sales add

--- crm.py
+++ crm.py
@@ -505,7 +505,7 @@ class CRMExtractor:
 
         # Скачивание отчета в эксель
         print('Нажимаю кнопку.')
-        time.sleep(4) 
+        time.sleep(6) 
         wait = WebDriverWait(self.driver, 30)
         menu_item = wait.until(
                     EC.element_to_be_clickable((
